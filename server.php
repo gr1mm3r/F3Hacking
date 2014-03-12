4696bfde6cd711cfa3541771686c23e486f7c225
gr1mm3r <ne123@ya.ru>
2014-03-12 07:34:06 +0300

    Update server.php
    Removed dependency on cpp files.

--- server.php
+++ server.php
@@ -1,22 +1,13 @@
 <?php
-	
-	if (!isset($_GET['length']) || !isset($_GET['count']))
-	{
-		echo "{}";
-		exit;
-	}
-	
-	$length = intval($_GET['length']);
-	$count = intval($_GET['count']);
-	
-	exec("./getwords {$length} {$count} 40", $results, $exitstatus);
-	
-	if ($exitstatus == 0)
-	{
-		echo $results[0];
-	}
-	else
-	{
-		echo "{}";
-	}
-?>
+	$diff= round(intval($_GET['length'])/2);	
+	$wordsarray = file('words/'.$_GET['length'].'.txt');
+	$passarray = array();
+	$endArr = array();
+	$correct = $wordsarray[rand(0,count($wordsarray)-1)];
+	foreach ($wordsarray as $elem){	if (levenshtein($correct, $elem) <= $diff){array_push($endArr, $elem);}}
+	for ($i=1;$i<= intval($_GET['count'])-1 ;$i++){
+		$fakepass = $endArr[rand(0,count($endArr)-1)];
+		array_push($passarray, $fakepass);}
+	$results = str_replace("\n",'',"{\"words\" : [\"".$correct."\", \"".implode('","', $passarray)."\" ]}");
+	$exitstatus = 1;
+	echo $results;
